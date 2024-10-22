b676a4a3885179f368ab08d299dd000befbeb5f4
VladimirOB <vovaso4@gmail.com>
2024-10-22 20:36:24 +0300

    first_become

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,3 @@ use Illuminate\Support\Facades\DB;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/vova', function () {
-    return response()->json("vova");
-});
-
-Route::get('/test', function () {
-    return response()->json("test");
-});
